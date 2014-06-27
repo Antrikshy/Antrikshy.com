f5b5afd72279ddd74540b1a7912206f71f739f64
Antriksh Yadav <Antrikshy@gmail.com>
2014-06-27 12:55:28 +0700

    All animations, mouseovers work in both browsers

--- About/about.js
+++ About/about.js
@@ -1,12 +1,11 @@
 $(document).ready(function() {
+    jQuery.support.cors = true;
+
     var aboutmeJSONReturn;
     var aboutmeJSONUrl = "https://api.about.me/api/v2/json/user/view/AntrikshYadav?apikey=a9b1086266608d1a73afdb4144ef97fdc6b9db7e";
     var bio = "";
     
     $.getJSON(aboutmeJSONUrl, function(aboutmeJSONReturn) {
-        console.log(aboutmeJSONReturn.bio);
-        bio = bio.replace(, "</p><p>");
-        console.log(aboutmeJSONReturn.bio);
         bio = "<p>" + aboutmeJSONReturn.bio + "</p>";
         $("#about_header").after($(bio).hide().fadeIn('slow'));
     });
--- script.js
+++ script.js
@@ -1,18 +1,4 @@
 $(document).ready(function() {
-    $('.header_button').hide().slideDown('slow');
     $('.content').delay(500).hide().fadeIn('slow');
-    
-    $('.header_button').mouseenter(function() {
-        $(this).clearQueue().animate({height: '140px'}, 'fast');
-    });
-    $('.header_button').mouseleave(function() {
-        $(this).clearQueue().animate({height: '130px'}, 'fast');
-    });
-
-    $('.home_button').mouseenter(function() {
-        $(this).clearQueue().animate({height: '150px'}, 'fast');
-    });
-    $('.home_button').mouseleave(function() {
-        $(this).clearQueue().animate({height: '140px'}, 'fast');
-    });
+    $('.header_button').parent().hide().slideDown('slow');
 });
