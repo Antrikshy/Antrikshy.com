0b244441e1abde6d9aae86ea45fa0a425ad13c12
Antriksh Yadav <Antrikshy@gmail.com>
2014-06-27 11:13:57 +0700

    Added all pages, header buttons work in Safari, not Firefox

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
-$().ready(function() {
+$(document).ready(function() {
+    $('.header_button').hide().slideDown('slow');
+    $('.content').delay(500).hide().fadeIn('slow');
     
+    $('.header_button').mouseenter(function() {
+        $(this).clearQueue().animate({height: '140px'}, 'fast');
+    });
+    $('.header_button').mouseleave(function() {
+        $(this).clearQueue().animate({height: '130px'}, 'fast');
+    });
+
+    $('.home_button').mouseenter(function() {
+        $(this).clearQueue().animate({height: '150px'}, 'fast');
+    });
+    $('.home_button').mouseleave(function() {
+        $(this).clearQueue().animate({height: '140px'}, 'fast');
+    });
 });
