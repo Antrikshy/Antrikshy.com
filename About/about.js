2de192162a948405e2962cd63eda4e0929de9116
Antriksh Yadav <Antrikshy@gmail.com>
2014-07-01 14:20:08 +0700

    Added basic projects page with design

--- About/about.js
+++ About/about.js
@@ -1,12 +1,21 @@
 $(document).ready(function() {
-    jQuery.support.cors = true;
-
-    var aboutmeJSONReturn;
-    var aboutmeJSONUrl = "https://api.about.me/api/v2/json/user/view/AntrikshYadav?apikey=a9b1086266608d1a73afdb4144ef97fdc6b9db7e";
-    var bio = "";
+    // var aboutmeJSONReturn;
+    // var aboutmeJSONUrl = "https://api.about.me/api/v2/json/user/view/AntrikshYadav?apikey=a9b1086266608d1a73afdb4144ef97fdc6b9db7e";
+    // var bio = "";
     
-    $.getJSON(aboutmeJSONUrl, function(aboutmeJSONReturn) {
-        bio = "<p>" + aboutmeJSONReturn.bio + "</p>";
-        $("#about_header").after($(bio).hide().fadeIn('slow'));
+    // $.getJSON(aboutmeJSONUrl, function(aboutmeJSONReturn) {
+    //     bio = "<p>" + aboutmeJSONReturn.bio + "</p>";
+    //     console.log(bio);
+    //     $("#about_header").after($(bio).hide().fadeIn('slow'));
+    // });
+
+    $.ajax({
+        url: 'https://api.about.me/api/v2/json/user/view/AntrikshYadav?client_id=a9b1086266608d1a73afdb4144ef97fdc6b9db7e',
+        type: 'GET',
+        dataType: 'json',
+        success: function (data) {
+            bio = "<p>" + data.bio + "</p>";
+            $("#about_header").after($(bio).hide().fadeIn('slow'));
+        }
     });
 });
