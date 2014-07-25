6a08cfbffbbd8644ca2d43b12972da91ae3a13c0
Antriksh Yadav <Antrikshy@gmail.com>
2014-07-25 15:43:36 +0700

    First selection of colors, fonts, styles
    Footer fixed, Montserrat and Roboto main fonts.

--- About/about.js
+++ About/about.js
@@ -1,21 +1,28 @@
 $(document).ready(function() {
-    // var aboutmeJSONReturn;
-    // var aboutmeJSONUrl = "https://api.about.me/api/v2/json/user/view/AntrikshYadav?apikey=a9b1086266608d1a73afdb4144ef97fdc6b9db7e";
-    // var bio = "";
-    
-    // $.getJSON(aboutmeJSONUrl, function(aboutmeJSONReturn) {
-    //     bio = "<p>" + aboutmeJSONReturn.bio + "</p>";
-    //     console.log(bio);
-    //     $("#about_header").after($(bio).hide().fadeIn('slow'));
-    // });
+    $("#about_header").after($("<p id=\"loading_msg\">Loading bio from about.me...<br /><br /></p>").fadeIn('fast').delay(2500));
+    $("#about_website_text").hide();
+    $("#about_website_header").hide();
 
     $.ajax({
         url: 'https://api.about.me/api/v2/json/user/view/AntrikshYadav?client_id=a9b1086266608d1a73afdb4144ef97fdc6b9db7e',
         type: 'GET',
-        dataType: 'json',
+        dataType: 'jsonp',
         success: function (data) {
             bio = "<p>" + data.bio + "</p>";
-            $("#about_header").after($(bio).hide().fadeIn('slow'));
+            bio = bio.replace(/\n/g, "</p><p>");
+
+            $("#loading_msg").fadeOut('fast');
+            $("#loading_msg").after($(bio).hide(0).fadeIn('slow'));
         }
     });
+
+    window.onscroll = function fadeInContent() {
+        var scrollTop = document.body.scrollTop;
+        if (scrollTop > 400) {
+            $("#about_website_header").fadeIn(400);
+        }
+        if (scrollTop > 600) {
+            $("#about_website_text").fadeIn(400);
+        }
+    }
 });
